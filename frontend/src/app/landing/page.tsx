f5acceed130acd23ee9bc69366877152a5334bfb
AdedamolaXL <adedamolaxl@outlook.com>
2024-04-14 22:54:42 +0100

    fix layout

--- frontend/src/app/landing/page.tsx
+++ frontend/src/app/landing/page.tsx
@@ -29,35 +29,40 @@ const Landing = () => {
 
   return (
     <>
-    <section className="absolute w-full h-full left-[0px] top-[120px]">
-      <div className='w-[1440px] h-[600px] left-[0px] top-[0px]'>
-        <h3 className='absolute w-[878px] h-[108px] left-[835px] top-[170px] font-bold text-[72px] leading-[108px]'>
+    <section className="py-28 relative bg-blue-600">
+      <div className='relative z-10 max-w-screen-xl mx-auto px-4 md:text-center md:px-8'>
+      <div className='max-w-xl md:mx-auto'>
+
+        <h3 className='text-white text-3xl font-semibold sm:text-4xl'>
           CLIMB
         </h3>
-        <p className="absolute w-[878px] h-[27px] left-[610px] top-[302px]">
+
+        <p className="text-blue-100 mt-3">
           Do you have a dream project, you have been putting off because you didn&apos;t have the time, money or help ?
         </p>
-        <p className='absolute w-[1078px] h-[27px] left-[880px] top-[352px]'>
+        <p className='text-blue-100 mt-3'>
           Well now, you can!. 
-        </p>      
-        <p className='absolute w-[1078px] h-[27px] left-[610px] top-[400px]'>
+        </p>    
+        <p className='text-blue-100 mt-3'>
           Climb to your goals from 0 - 100. Stack your earnings 0 -100. Find your first 100 people. Start with 1 project.
-        </p>  
-        <p className='absolute w-[1078px] h-[27px] left-[855px] top-[450px]'>
+        </p> 
+        <p className='text-blue-100 mt-3'>
           It&apos;s time to bet on yourself!
         </p> 
       </div>
-
-      <div className="absolute w-[306px] h-[58px] left-[897px] top-[500px]">
-        <button className='px-10 py-3.5 text-white bg-indigo-600 rounded-lg duration-150 hover:bg-indigo-700 active:shadow-lg'>
+        
+      <div className="mt-4">
+        <button className='inline-block py-2 px-4 text-gray-800 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 rounded-full'>
           <Link className='hover:underline' href={redirectUrl}>
             Sign Up
           </Link>
         </button>
       </div>
-      
-                                              
- 
+
+      </div>
+      <div className="absolute top-0 w-full h-full" 
+        style={{ background: "linear-gradient(268.24deg, rgba(59, 130, 246, 0.76) 50%, rgba(59, 130, 246, 0.545528) 80.61%, rgba(55, 48, 163, 0) 117.35%)" }}>
+      </div>
     </section>
     </>
   )
